7a6098abc13c974453c86b249e7753c9fbf81967
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2022-01-05 17:51:51 -0800

    Add POD documentation and --help option.

--- multi-git-log.pl
+++ multi-git-log.pl
@@ -4,6 +4,7 @@ use warnings;
 #use File::Find;
 use Git;
 use Getopt::Long;
+use Pod::Usage;
 use Data::Dumper;
 sub parseCommits {
     my @commits;
@@ -30,8 +31,13 @@ sub date {
 }
 # --author, --committer from `git log`
 my ($author, $committer);
-GetOptions("author=s", \$author,
-           "committer=s", \$committer);
+my ($help, $man) = 0;
+GetOptions("author=s" => \$author,
+           "committer=s" => \$committer,
+	   "help|?" => \$help,
+           "man" => \$man);
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
 my $dir = shift;
 my @gitdirs = map {s/.git$//;$_} glob "$dir/*/.git";
 my @allcommits = ();
@@ -71,3 +77,38 @@ foreach my $repodir (@gitdirs) {
     }
 }
 print map {$_->[0]} sort {$b->[1] cmp $a->[1]} map {[$_, date($_)]} @allcommits;
+__END__
+
+=head1 NAME
+
+multi-git-log.pl - View your git commits across multiple repositories
+
+=head1 SYNOPSIS
+
+multi-git-log.pl --author="Your name" ~/src
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--author>
+Shows commits authored by a certain author.
+
+=item B<--committer>
+Shows commits whose committer field matches the following argument.
+
+=item B<--help>
+
+Outputs usage message.
+
+=item B<--man>
+
+Prints this pod document.
+
+=back
+
+=head1 DESCRIPTION
+
+This program shows your (or someone else's) commits in multiple repositories. The --author and --committer options tell the program which commits to show.
+
+=cut
